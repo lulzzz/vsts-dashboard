385c5c2e478dfa87c4e0813aed562b1612eda944
Mark Gaze <mark.l.gaze@gmail.com>
2018-05-17 12:06:50 +0100

    Added bootstrap navbar to the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './header.css';
 
 export default class Header extends React.Component {
+  private title: string = 'VSTS Dashboard';
   public render() {
     return (
       <div>
-        <h1>Dashboard</h1>
+        <Navbar color="dark" dark={true} expand="md">
+          <NavbarBrand href="/">{this.title}</NavbarBrand>
+        </Navbar>
       </div>
     );
   }
