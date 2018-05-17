62a7e18411f38a67d5915862fdb71457caa3fad8
Mark Gaze <mark.l.gaze@gmail.com>
2018-05-17 11:10:20 +0100

    Added initial header component files

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './header.css';
+
+export default class Header extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    );
+  }
+}
