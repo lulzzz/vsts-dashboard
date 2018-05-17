250d020f3cfa566224ac6144de0a5081026f344e
Mark Gaze <mark.l.gaze@gmail.com>
2018-05-17 10:52:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dashboard
+
+A dashboard to show the status of VSTS builds
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Dashboard</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
