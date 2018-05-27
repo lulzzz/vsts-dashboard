53589db6a0dfb4f52a1bad73b5cf2d46d6dce8e5
Mark G <mark.l.gaze@gmail.com>
2018-05-27 21:48:17 +0100

    Refactored to use local storage and added docs

--- src/services/config-service.ts
+++ src/services/config-service.ts
@@ -1,13 +1,65 @@
 export class ConfigService {
+  private readonly tokenKey: string = 'VSTS_TOKEN';
+  private readonly urlKey: string = 'VSTS_URL';
+  private readonly projectKey: string = 'VSTS_PROJECT';
+
+  /**
+   * Gets the authentication token for VSTS
+   */
   public get Token(): string {
-    return process.env.VSTS_TOKEN as string;
+    return this.getFromStorage(this.tokenKey);
   }
 
+  /**
+   * Sets the authentication token for VSTS
+   */
+  public set Token(value: string) {
+    this.saveToStorage(this.tokenKey, value);
+  }
+
+  /**
+   * Gets the VSTS account URL
+   */
   public get AccountURL(): string {
-    return process.env.VSTS_URL as string;
+    return this.getFromStorage(this.urlKey);
   }
 
+  /**
+   * Sets the VSTS account URL
+   */
+  public set AccountURL(value: string) {
+    this.saveToStorage(this.urlKey, value);
+  }
+
+  /**
+   * Gets the VSTS project name
+   */
   public get ProjectName(): string {
-    return process.env.VSTS_PROJECT as string;
+    return this.getFromStorage(this.projectKey);
+  }
+
+  /**
+   * Sets the VSTS project name
+   */
+  public set ProjectName(value: string) {
+    this.saveToStorage(this.projectKey, value);
+  }
+
+  /**
+   * Gets a string from the local storage with the
+   * key passed to it
+   * @param key The key to get the data from
+   */
+  private getFromStorage(key: string): string {
+    return localStorage.getItem(key) as string;
+  }
+
+  /**
+   * Stores a string in the local storage cache
+   * @param key The key to store the data under
+   * @param value The string value to store
+   */
+  private saveToStorage(key: string, value: string) {
+    localStorage.setItem(key, value);
   }
 }
