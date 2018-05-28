a9d99c77ca035af6b5c109fd707a4d7dc9a97058
Mark G <mark.l.gaze@gmail.com>
2018-05-28 18:27:13 +0100

    Mock out the localStorage so that the tests still pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,30 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+class LocalStorageMock {
+  private store: any;
+
+  constructor() {
+    this.store = {};
+  }
+
+  public clear() {
+    this.store = {};
+  }
+
+  public getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  public setItem(key: string, value: string) {
+    this.store[key] = value.toString();
+  }
+
+  public removeItem(key: string) {
+    delete this.store[key];
+  }
+}
+
+// @ts-ignore
+global.localStorage = new LocalStorageMock();
